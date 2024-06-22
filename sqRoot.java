e88144584e20c2384c847f6bf661516e7b7fcebf
Harshiniyella <153215195+Harshiniyella@users.noreply.github.com>
2024-06-22 19:10:24 +0530

    Add files via upload

---
+++ sqRoot.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class sqRoot
+{
+public static void main(String args[])
+{
+       Scanner s=new Scanner(System.in);
+       int a;
+       a=s.nextInt();
+       double b=Math.sqrt(a);
+       System.out.println(b);
+}
+}
